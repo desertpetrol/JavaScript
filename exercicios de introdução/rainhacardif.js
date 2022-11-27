f3e2223996bcb122b9d19a0e6cd0949543e95fbf
Pedro E <55996330+desertpetrol@users.noreply.github.com>
2022-11-27 16:36:00 -0300

    Add files via upload

---
+++ exercicios de introdução/rainhacardif.js
@@ -0,0 +1,10 @@
+//No reino unido ao fazer 100 anos você ganha uma carta da rainha.
+let age = 101
+
+if (age < 100) {
+    console.log('Sem carta para você!')
+} else if (age === 100) { 
+    console.log('Aqui está sua carta!')
+} else {
+    console.log('Você já tem um cartão!')
+}
